0cf38e0cec0452275248330c444c771623c5d68d
poliam <william.crumb@vikingassetmanagement.com>
2023-04-14 21:07:21 +0800

    update the navbar

--- static/portal/js/portalnavbar.js
+++ static/portal/js/portalnavbar.js
@@ -33,29 +33,58 @@
 	}
 
 
-
-	$(".main-container").hover(function(){
+	$("#sidebar-container").hover(function(){
 		$('.menu-collapsed').toggleClass('d-none');
 	    $('.sidebar-submenu').toggleClass('d-none');
 	    $('.submenu-icon').toggleClass('d-none');
 	    $('#sidebar-container').toggleClass('sidebar-expanded sidebar-collapsed');
 	    // Treating d-flex/d-none on separators with title
 	    var SeparatorTitle = $('.sidebar-separator-title');
+	    var SampleSeparatorTitle = $('#sidebar-container');
 	    if (SeparatorTitle.hasClass('d-flex') ) {
-	       	
+	       	$(".navbar-icons").css("margin-left", "30%");
 	    } else {
-	    	console.log("here2");
 	        SeparatorTitle.addClass('d-flex');
-	        $(".logo_full").hide();
-	        $(".logo_small").show();
-	        $(".navbar-icons").css("margin-left", "30%");
+	        if(SampleSeparatorTitle.hasClass('sidebar-expanded')){
+	        	$(".logo_full").show();
+		        $(".logo_small").hide();
+		        $(".navbar-icons").css("margin-left", "0%");
+	        }else{
+	        	$(".logo_full").hide();
+		        $(".logo_small").show();
+		        $(".navbar-icons").css("margin-left", "30%");
+	        }
+	        // console.log()
+	        // $(".logo_full").show();
+	        // $(".logo_small").hide();
+	        // $(".navbar-icons").css("margin-left", "0%");
 	    }
 	    // Collapse/Expand icon
 	    $('#collapse-icon').toggleClass('fa-angle-double-left fa-angle-double-right');
 	})
 
-	$(".main-container").mouseout(function(){
-		$(".logo_full").show();
-        $(".logo_small").hide();
-        $(".navbar-icons").css("margin-left", "0%");
-	})
+
+	// $(".main-container").hover(function(){
+	// 	$('.menu-collapsed').toggleClass('d-none');
+	//     $('.sidebar-submenu').toggleClass('d-none');
+	//     $('.submenu-icon').toggleClass('d-none');
+	//     $('#sidebar-container').toggleClass('sidebar-expanded sidebar-collapsed');
+	//     // Treating d-flex/d-none on separators with title
+	//     var SeparatorTitle = $('.sidebar-separator-title');
+	//     if (SeparatorTitle.hasClass('d-flex') ) {
+	       	
+	//     } else {
+	//         SeparatorTitle.addClass('d-flex');
+	//         $(".logo_full").hide();
+	//         $(".logo_small").show();
+	//         $(".navbar-icons").css("margin-left", "30%");
+	//     }
+	//     // Collapse/Expand icon
+	//     $('#collapse-icon').toggleClass('fa-angle-double-left fa-angle-double-right');
+	// })
+
+	// $("#sidebar-container").mouseout(function(){
+	// 	$(".logo_full").hide();
+    //     $(".logo_small").show();
+    //     $(".navbar-icons").css("margin-left", "30%");
+	// })
